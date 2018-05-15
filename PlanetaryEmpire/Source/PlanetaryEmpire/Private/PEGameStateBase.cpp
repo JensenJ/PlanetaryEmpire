b40eb144841f0188a045aac024139592512f1c35
JensenJ <jensenbradley@icloud.com>
2018-05-15 21:03:27 +0100

    Time system logic implemented and logging out to console

--- PlanetaryEmpire/Source/PlanetaryEmpire/Private/PEGameStateBase.cpp
+++ PlanetaryEmpire/Source/PlanetaryEmpire/Private/PEGameStateBase.cpp
@@ -1,7 +1,59 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "PEGameStateBase.h"
+#include "Math/UnrealMathUtility.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "Kismet/KismetStringLibrary.h"
+#include "GenericPlatform/GenericPlatformMath.h"
 
+APEGameStateBase::APEGameStateBase() {
+	PrimaryActorTick.bCanEverTick = true;
+}
 
+void APEGameStateBase::BeginPlay() {
+	GameTime.SetNum(3); // Limits size of array to 3
+}
 
+void APEGameStateBase::Tick(float DeltaSeconds) {
+	SetClockwork(DeltaSeconds);
+	Clock();
+}
 
+void APEGameStateBase::SetClockwork(float DeltaSeconds) {
+	float DeltaTimeUnit = DeltaSeconds / TimeUnit;
+	float AddedClockwork = DeltaTimeUnit + Clockwork;
+	int NewDayTick = AddedClockwork / (60 * 24);
+	float Remainder = FGenericPlatformMath::Fmod(AddedClockwork, (60.0f * 24.0f));
+	Clockwork = Remainder;
+	DayTick = NewDayTick;
+}
+
+void APEGameStateBase::Clock() {
+	// Seconds
+	float ClockworkSeconds = Clockwork * 3600;
+	float NewSeconds = ClockworkSeconds / 60;
+	float RemainderSeconds = FGenericPlatformMath::Fmod(NewSeconds, 60.0f);
+	UKismetMathLibrary::FFloor(RemainderSeconds);
+	Seconds = RemainderSeconds;
+
+	// Minutes
+	float NewMinutes = NewSeconds / 60;
+	float RemainderMinutes = FGenericPlatformMath::Fmod(NewMinutes, 60.0f);
+	UKismetMathLibrary::FFloor(RemainderMinutes);
+	Minutes = RemainderMinutes;
+
+	// Hours
+	float NewHours = NewMinutes / 60;
+	float RemainderHours = FGenericPlatformMath::Fmod(NewHours, 60.0f);
+	UKismetMathLibrary::FFloor(RemainderHours);
+	Hours = RemainderHours;
+
+	GameTime.Insert(Seconds, 0);
+	GameTime.Insert(Minutes, 1);
+	GameTime.Insert(Hours, 2);
+
+	FString strHours = FString::FromInt(GameTime[2]);
+	FString HoursMinutesString = UKismetStringLibrary::BuildString_Int(strHours, ":", GameTime[1], "");
+	FString FinalString = UKismetStringLibrary::BuildString_Int(HoursMinutesString, ":", GameTime[0], "");
+	UE_LOG(LogTemp, Warning, TEXT("PEGameStateBase: Time: %s"), *FinalString);
+}
--- PlanetaryEmpire/Source/PlanetaryEmpire/Public/PEGameStateBase.h
+++ PlanetaryEmpire/Source/PlanetaryEmpire/Public/PEGameStateBase.h
@@ -13,4 +13,28 @@ UCLASS()
 class PLANETARYEMPIRE_API APEGameStateBase : public AGameStateBase
 {
 	GENERATED_BODY()
+
+public:
+	APEGameStateBase();
+protected:
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaSeconds) override;
+
+private:
+
+	// Clock Functions
+	void SetClockwork(float DeltaSeconds);
+	void Clock();
+	// Clock Variables
+	float TimeUnit = 1.0f;
+	float Clockwork = 0.0f;
+	float DayTick = 0;
+	int32 Seconds = 0;
+	int32 Minutes = 0;
+	int32 Hours = 0;
+	int32 Day = 1;
+	int32 Month = 1;
+	int32 Year = 1;
+	TArray<int32> GameTime;
+	TArray<int32> GameDate;
 };
